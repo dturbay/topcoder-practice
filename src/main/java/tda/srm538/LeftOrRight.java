b58ed2c12bf5746138ca88a83245af74e88cd72a
Dennis Turbay <dennis.turbay@gmail.com>
2015-01-21 21:38:43 -0800

    starting practice)

---
+++ src/main/java/tda/srm538/LeftOrRight.java
@@ -0,0 +1,33 @@
+package tda.srm538;
+
+
+/**
+ * problem:
+ * http://community.topcoder.com/stat?c=round_overview&rd=14729
+ */
+
+public class LeftOrRight {
+    public int maxDistance(String program) {
+        return Math.max(maxDistance(program, 1), maxDistance(program, -1));
+    }
+
+    private int maxDistance(String program, int step) {
+        int maxDist = 0;
+        int dist = 0;
+        for (char ch: program.toCharArray()) {
+            switch (ch) {
+                case 'R':
+                    dist += 1;
+                    break;
+                case 'L':
+                    dist -= 1;
+                    break;
+                case '?':
+                    dist += step;
+                    break;
+            }
+            maxDist = Math.max(maxDist, Math.abs(dist));
+        }
+        return maxDist;
+    }
+}
---
+++ src/main/java/tda/srm538/SkewedPerspectives.java
@@ -0,0 +1,13 @@
+package tda.srm538;
+
+/**
+ * problem:
+ * http://community.topcoder.com/stat?c=round_overview&rd=14729
+ */
+public class SkewedPerspectives {
+
+    public String[] areTheyPossible(int[] cubes, int B, int w, String[] views) {
+        return null;
+    }
+
+}
