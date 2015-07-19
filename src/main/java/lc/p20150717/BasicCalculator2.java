77926030bb090c479e89024d93710fad7595beb3
Dennis Turbay <dennis.turbay@gmail.com>
2015-07-18 19:40:00 -0700

    added new problem solvings

--- src/main/java/lc/p20150717/BasicCalculator2.java
+++ src/main/java/lc/p20150717/BasicCalculator2.java
@@ -10,6 +10,11 @@ import java.util.Set;
  * The expression string contains only non-negative integers, +, -, *, / operators and empty spaces.
  * The integer division should truncate toward zero.
  * You may assume that the given expression is always valid.
+ *
+ * Some examples:
+ * "3+2*2" = 7
+ " 3/2 " = 1
+ " 3+5 / 2 " = 5
  */
 public class BasicCalculator2 {
 
